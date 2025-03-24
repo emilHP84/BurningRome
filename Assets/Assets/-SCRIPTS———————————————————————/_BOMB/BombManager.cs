17e4cbd6ac4e77b3a0b23e66df4ce47de6f4e5ae
emil <ehp84@sfr.fr>
2025-03-24 14:32:44 +0100

    ajout destruction bombe sans explosion

--- Assets/Assets/-SCRIPTS———————————————————————/_BOMB/BombManager.cs
+++ Assets/Assets/-SCRIPTS———————————————————————/_BOMB/BombManager.cs
@@ -1,4 +1,5 @@
 using Rewired;
+using System.Collections;
 using UnityEngine;
 
 public class BombManager : MonoBehaviour, ICollisionable
@@ -6,11 +7,16 @@ public class BombManager : MonoBehaviour, ICollisionable
     private Rigidbody rb;
     private SphereCollider sphereCollider;
 
+    [SerializeField][Range(0,6)] float m_delayBetweenExplose;
+    [SerializeField][Range(0,3)] float m_delayExplose;
 
+    private float time;
+    private bool HasExplose;
     
     void Start()
     {
         SetComponent();
+        time = 0;
     }
     void SetComponent()
     {
@@ -20,9 +26,20 @@ public class BombManager : MonoBehaviour, ICollisionable
     
     void Update()
     {
-        
+        time += Time.deltaTime;
+        if (HasExplose == false && time >= m_delayBetweenExplose ) 
+        {
+            StartCoroutine(Explose());
+            HasExplose = true;
+            time = 0;
+        }
     }
 
+    IEnumerator Explose()
+    {
+        yield return new WaitForSeconds(m_delayExplose);
+        Destroy(this.gameObject);
+    }
 
     // detecte les différent objet en collision ( nécésite un rigidbody 
     public void OnCollisionWith(ICollisionable collisionable)
