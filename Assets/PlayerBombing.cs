209a2aeb163a0a3fa2173da258dc7bdb71aab276
Karl (Hackintosh) <>
2025-05-01 20:36:39 +0200

    Les bombes manuelles c'est ok, les power up c'est pret aussi

--- Assets/PlayerBombing.cs
+++ Assets/PlayerBombing.cs
@@ -7,12 +7,17 @@ public class PlayerBombing : MonoBehaviour
     [SerializeField] int playerID = 0;
     Player player;
     bool canBomb = false;
+    public int MaxBomb{get{return maxBomb;}}
     [SerializeField] int maxBomb = 1;
     int remainingBomb = 1;
 
     [SerializeField] GameObject bombPrefab;
-    List<GameObject> activeBombs = new List<GameObject>();
+    List<BombManager> activeBombs = new List<BombManager>();
     [SerializeField] GameObject bombFx, noBombFx;
+    [SerializeField] private int explosionRange = 1;
+    public int ExplosionRange => explosionRange;
+    int manualDetonation = 0;
+    BombManager manualBomb;
 
     void Update()
     {
@@ -22,7 +27,23 @@ public class PlayerBombing : MonoBehaviour
             {
                 if (remainingBomb>0)
                 {
-                    NewBomb();
+                    if (manualDetonation<1) NewBomb();
+                    else
+                    {
+                        if(manualDetonation<2)
+                        {
+                            if (manualBomb!=null) manualBomb.Explode();
+                            else NewBomb();
+                            manualDetonation = 0;
+                        }
+                        else
+                        {
+                            NewBomb();
+                            manualBomb = activeBombs[activeBombs.Count-1];
+                            manualBomb.ChangeBombState(true);
+                            manualDetonation = 1;
+                        }
+                    }
                 }
                 else
                 {
@@ -39,8 +60,6 @@ public class PlayerBombing : MonoBehaviour
                 activeBombs.RemoveAt(i);
             }
         }
-
-
     }
 
     void NewBomb()
@@ -50,7 +69,9 @@ public class PlayerBombing : MonoBehaviour
         bombPos.x = Mathf.Round(bombPos.x);
         bombPos.z = Mathf.Round(bombPos.z);
         bombPos.y = 2f;
-        activeBombs.Add(Instantiate(bombPrefab,bombPos,transform.rotation));
+        BombManager newBomb = Instantiate(bombPrefab,bombPos,transform.rotation).GetComponent<BombManager>();
+        activeBombs.Add(newBomb);
+        newBomb.GetComponent<BombManager>().SetExplosionRange(explosionRange);
         remainingBomb--;
         if (bombFx) Instantiate(bombFx,bombPos,transform.rotation);
     }
@@ -83,16 +104,28 @@ public class PlayerBombing : MonoBehaviour
 
     void ResetBombs()
     {
-        activeBombs = new List<GameObject>();
+        activeBombs = new List<BombManager>();
         remainingBomb = maxBomb;
     }
 
     public void ChangeMaxBomb(int desired)
     {
-        if (desired<0) desired = 0;
+        if (desired<1) desired = 1;
         maxBomb = desired;
         if (remainingBomb>maxBomb) remainingBomb=maxBomb;
-        if (activeBombs.Count>maxBomb) activeBombs = new List<GameObject>();
+        if (activeBombs.Count>maxBomb) activeBombs = new List<BombManager>();
     }
+
+    public void ChangeRange(int desired)
+    {
+        explosionRange = Mathf.Clamp(desired,1,99);
+    }
+
+    public void AddManualDetonation()
+    {
+        manualDetonation=2;
+    }
+
+    
 
 } // FIN DU SCRIPT
