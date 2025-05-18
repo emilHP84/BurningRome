f622faf83b56069aacbb80fb24280a4d01ace705
Abdou Mouaad <shligom.bongonb34@gmail.com>
2025-05-18 05:30:11 +0200

    collider bombe

--- burningRome/Assets/-SCRIPTS———————————————————————/GAMEPLAY/SCR_Component/CollsionComponent.cs
+++ burningRome/Assets/-SCRIPTS———————————————————————/GAMEPLAY/SCR_Component/CollsionComponent.cs
@@ -2,10 +2,24 @@ using UnityEngine;
 
 public class CollsionComponent : MonoBehaviour
 {
+
     private void OnCollisionEnter(Collision collision)
     {
         ICollisionable obj = GetComponent<ICollisionable>();
         if (collision.gameObject.GetComponent<ICollisionable>() is null) return;
         obj.OnCollisionWith(collision.gameObject.GetComponent<ICollisionable>());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        PlayerMovement input = other.gameObject.GetComponent<PlayerMovement>();
+        if (input != null)
+        {
+            Collider col = GetComponent<Collider>();
+            if (col != null)
+            {
+                col.isTrigger = false;
+            }
+        }
     }
 }
