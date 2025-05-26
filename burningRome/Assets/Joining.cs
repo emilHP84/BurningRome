171ea2fcdb8df2ce0f8ad6f1f91a32c3dc158922
Karl (Hackintosh) <>
2025-05-26 22:47:46 +0200

    Menu de Joining qui cache le texte PRESS BUTTON et affiche le bouton DÉMARRER quand le deuxieme joueur Join

--- burningRome/Assets/Joining.cs
+++ burningRome/Assets/Joining.cs
@@ -1,25 +1,33 @@
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class Joining : MonoBehaviour
 {
     Vector3 startScale;
+    [SerializeField] Button startBattleButton;
+    [SerializeField] GameObject pressButtonText;
 
     void Awake()
     {
         startScale = transform.localScale;
+        startBattleButton.gameObject.SetActive(false);
+        pressButtonText.gameObject.SetActive(false);
     }
 
     private void OnEnable()
     {
         EVENTS.OnJoiningStart += Display;
         EVENTS.OnJoiningDone += UnDisplay;
+        EVENTS.OnPlayerSpawn += CheckPlayerQuantity;
+        if (GAMEPLAY.access.CurrentState == GameplayState.joining) Display();
     }
 
     private void OnDisable()
     {
         EVENTS.OnJoiningStart -= Display;
         EVENTS.OnJoiningDone -= UnDisplay;
+        EVENTS.OnPlayerSpawn -= CheckPlayerQuantity;
     }
 
     public void UnDisplay()
@@ -30,6 +38,8 @@ public class Joining : MonoBehaviour
 
     void Display()
     {
+        startBattleButton.gameObject.SetActive(false);
+        pressButtonText.gameObject.SetActive(true);
         transform.DOKill(); // security to avoid multiple dotween
         transform.DOScale(startScale, 0.5f).From(0).SetEase(Ease.OutCirc);
     }
@@ -39,6 +49,15 @@ public class Joining : MonoBehaviour
     {
         gameObject.SetActive(false); // hide
         transform.localScale = startScale; // reset scale to max
+    }
+
+    void CheckPlayerQuantity(int playerID)
+    {
+        if (GAMEPLAY.access.TotalPlayers > 0)
+        {
+            startBattleButton.gameObject.SetActive(true);
+            pressButtonText.gameObject.SetActive(false);
+        }
     }
 
   
